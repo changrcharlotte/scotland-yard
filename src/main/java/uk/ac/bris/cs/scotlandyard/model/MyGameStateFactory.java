cf89f8ea2c118b65ad91e84ee88f4561f35f6c07
charlotte <charlotte_r_chang@outlook.com>
2026-03-11 11:52:08 +0000

    added initial gamestate

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,11 +1,18 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 import jakarta.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.*;
+import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import uk.ac.bris.cs.scotlandyard.model.Move.*;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 
 /**
  * cw-model
@@ -18,8 +25,30 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			Player mrX,
 			ImmutableList<Player> detectives) {
 		// TODO
-		throw new RuntimeException("Implement me!");
-
+		return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
+//		throw new RuntimeException("Implement me!");
 	}
+
+	private final class MyGameState implements GameState {
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
+
+		MyGameState(GameSetup st, ImmutableSet<Piece> x, ImmutableList<LogEntry> lg, Player mrx, List<Player> dt){
+			setup = st;
+			remaining = x;
+			log = lg;
+			mrX = mrx;
+			detectives = dt;
+		}
+		@Override public GameSetup getSetup() {  return null; }
+		@Override  public ImmutableSet<Piece> getPlayers() { return null; }
+		@Override public GameState advance(Move move) {  return null;  }
+	}
+
 
 }
